5d073d7ec09e41822a46b6e0401de9ab3b5c53ad
Math <malusrocha2000@gmail.com>
2025-05-16 11:25:06 +0000

    Pokemon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
+import poke from './assests/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
 
-
-const Home = () =>{
+const Home = () => {
 
     return (
         <div>
-            <p>POKEMON</p>
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
         </div>
     )
 }
